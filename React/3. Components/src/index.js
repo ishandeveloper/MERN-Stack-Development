2ed847c53387af3f9e98ab6be1c5dc6ba0653e23
Ishan Sharma <ishandeveloper@outlook.com>
2020-04-29 18:43:59 +0530

    Components

--- React/3. Components/src/index.js
+++ React/3. Components/src/index.js
@@ -1,12 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Heading from "./components/heading";
-import UList from "./components/list";
+import App from "./app";
 
 ReactDOM.render(
   <div>
-    <Heading />
-    <UList/>
+    <App />
   </div>,
   document.getElementById("root")
 );
