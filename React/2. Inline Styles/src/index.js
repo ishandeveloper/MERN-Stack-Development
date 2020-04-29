a522a54d54b6c77c6d825677a696d81826085ba5
Ishan Sharma <ishandeveloper@outlook.com>
2020-04-29 17:34:30 +0530

    Inline Styles

---
+++ React/2. Inline Styles/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+var greetings="Good Morning!";
+var style = {
+    color: "red",
+    fontSize: "3em",
+    textAlign: "center",
+    height:"100px",
+    margin:"25% 0",
+    
+    borderBottom:"5px solid black"
+}
+    const time=new Date().getHours();
+    time <= 12 ? ((greetings = "Good Morning !") && (style.color = "red")) : time <= 18 ? ((greetings = "Good Afternoon !") && (style.color = "blue")) : ((greetings = "Good Evening !" && (style.color = "green")));
+ReactDOM.render(
+<h1 style={style}>{greetings}</h1>,
+document.getElementById('root')
+);
