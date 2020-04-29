1ee6f1718362ffb98afbefaa0e014561ed64ea78
Ishan Sharma <ishandeveloper@outlook.com>
2020-04-29 23:30:53 +0530

    Modules

--- React/4. Modules/src/index.js
+++ React/4. Modules/src/index.js
@@ -1,12 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import pi, {doublePi,triplePi} from './math';
+
+// We can import only necessary functions one by one using following syntax
+// import pi, {doublePi,triplePi} from './math';
+
+// To import all the functions at once, we can use
+import * as pi from './math';
 
 ReactDOM.render(
   <div>
-    <li>{pi}</li>
-    <li>{doublePi()}</li>
-    <li>{triplePi()}</li>
+    <li>{pi.default}</li>
+    <li>{pi.doublePi()}</li>
+    <li>{pi.triplePi()}</li>
   </div>,
   document.getElementById("root")
 );
