fd726bdeb775b7f78eabac6f09ceee2c8ba923dd
Ishan Sharma <ishandeveloper@outlook.com>
2020-04-29 23:30:43 +0530

    Modules

--- React/4. Modules/src/components/heading.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Heading() {
-  return <h1>Hey, I'm A Component ! </h1>;
-}
-
-export default Heading;
--- React/4. Modules/src/index.js
+++ React/4. Modules/src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./app";
+import pi, {doublePi,triplePi} from './math';
 
 ReactDOM.render(
   <div>
-    <App />
+    <li>{pi}</li>
+    <li>{doublePi()}</li>
+    <li>{triplePi()}</li>
   </div>,
   document.getElementById("root")
 );
