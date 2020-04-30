e155abd8b8697a50161f2c83e32a9594af4a8b90
Ishan Sharma <ishandeveloper@outlook.com>
2020-05-01 02:44:33 +0530

    App

---
+++ React/8.hooks/src/app.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function App() {
+    return <div className="container">
+        <h1 className="count">0</h1>
+        <button className="count-btn">+</button>
+    </div>
+}
+export default App;
