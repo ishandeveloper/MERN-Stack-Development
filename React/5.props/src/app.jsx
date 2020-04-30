8164637d83a10b92b147c249cc29f66c3b86e558
Ishan Sharma <ishandeveloper@outlook.com>
2020-04-30 12:47:55 +0530

    Props

---
+++ React/5.props/src/app.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Avatar from './components/avatar';
+
+function App() {
+  return <ul className="collection">
+      <Avatar title="Alisha" subtitle="alisha@example.com"description="Mumbai, India"/>
+  </ul>;
+}
+
+export default App;
--- React/5.props/src/index.js
+++ React/5.props/src/index.js
@@ -1,17 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-// We can import only necessary functions one by one using following syntax
-// import pi, {doublePi,triplePi} from './math';
-
-// To import all the functions at once, we can use
-import * as pi from './math';
+import App from './app';
 
 ReactDOM.render(
   <div>
-    <li>{pi.default}</li>
-    <li>{pi.doublePi()}</li>
-    <li>{pi.triplePi()}</li>
+    <App />
   </div>,
   document.getElementById("root")
 );
