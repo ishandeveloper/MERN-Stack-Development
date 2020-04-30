4d93d919d1ea7bd9a0a3936b3f726b08fd5957b3
Ishan Sharma <ishandeveloper@outlook.com>
2020-04-30 22:35:26 +0530

    Message

---
+++ React/7.conditional-rendering/src/components/message.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Message() {
+    return <div>
+        <h1>Hey, There!</h1>
+        <p>You're now successfully logged in.</p>
+    </div>
+}
+
+export default Message;
