b13707e961a24e91d5571de5f1c897c3ddb30c57
Ishan Sharma <ishandeveloper@outlook.com>
2020-05-01 03:04:49 +0530

    Final

--- React/8.hooks/src/app.jsx
+++ React/8.hooks/src/app.jsx
@@ -6,10 +6,16 @@ var [count,setCount]=React.useState(0);
   function increase() {
     setCount(++count);
   }
+  function decrease() {
+    setCount(--count);
+  }
 
   return (
     <div className="container">
       <h1 className="count">{count}</h1>
+      <button onClick={decrease} className="count-btn">
+        -
+      </button>
       <button onClick={increase} className="count-btn">
         +
       </button>
