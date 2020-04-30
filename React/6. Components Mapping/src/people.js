607200aa1fa29f0fd4e23142d608d607c45d9d64
Ishan Sharma <ishandeveloper@outlook.com>
2020-04-30 13:51:09 +0530

    Data array

---
+++ React/6. Components Mapping/src/people.js
@@ -0,0 +1,28 @@
+const people = [
+  {
+    id: 1,
+    title: "Alisha",
+    subtitle: "alisha@example.com",
+    description: "Mumbai, India",
+  },
+  {
+    id: 2,
+    title: "Andrew",
+    subtitle: "andrew@example.com",
+    description: "New York, USA",
+  },
+  {
+    id: 3,
+    title: "John",
+    subtitle: "john@example.com",
+    description: "Phillipines, Indonesia",
+  },
+  {
+    id: 4,
+    title: "Chirag",
+    subtitle: "chirag@example.com",
+    description: "Delhi, India",
+  },
+];
+
+export default people;
