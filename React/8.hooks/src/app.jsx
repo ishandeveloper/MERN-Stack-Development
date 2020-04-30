74b6dba7eb24a6a056a386db84a0f242c84cb609
Ishan Sharma <ishandeveloper@outlook.com>
2020-05-01 03:01:17 +0530

    Final

--- React/8.hooks/src/app.jsx
+++ React/8.hooks/src/app.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React from "react";
 
 function App() {
-    return <div className="container">
-        <h1 className="count">0</h1>
-        <button className="count-btn">+</button>
+var [count,setCount]=React.useState(0);
+
+  function increase() {
+    setCount(++count);
+  }
+
+  return (
+    <div className="container">
+      <h1 className="count">{count}</h1>
+      <button onClick={increase} className="count-btn">
+        +
+      </button>
     </div>
+  );
 }
 export default App;
