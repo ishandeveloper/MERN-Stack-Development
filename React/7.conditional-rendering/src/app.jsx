bede626ef6a8f189e724728992b3a5815da5ac5d
Ishan Sharma <ishandeveloper@outlook.com>
2020-04-30 22:35:00 +0530

    Main App

---
+++ React/7.conditional-rendering/src/app.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Login from "./components/login";
+import Message from './components/message';
+function App() {
+  return (
+    <div className="text-center">
+      {/* <Login /> */}
+      <Message/>
+    </div>
+  );
+}
+
+export default App;
