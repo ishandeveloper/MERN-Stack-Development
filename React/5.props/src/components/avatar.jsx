50e271f267745b213d22d7281387411b43bb17f1
Ishan Sharma <ishandeveloper@outlook.com>
2020-04-30 12:47:36 +0530

    avatar

---
+++ React/5.props/src/components/avatar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Avatar(props) {
+    return  <li class="collection-item avatar">
+    <img src="https://source.unsplash.com/100x100?person" alt="" class="circle" />
+    <span class="title">{props.title}</span>
+    <p>{props.subtitle}<br/>
+       {props.description}
+    </p>
+    <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
+  </li>
+}
+
+export default Avatar;
