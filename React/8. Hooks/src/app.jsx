051d7634d082e25998ec165df906c2c97f7c3d6f
Ishan Sharma <ishandeveloper@outlook.com>
2020-05-03 16:27:14 +0530

    Hooks

---
+++ React/8. Hooks/src/app.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function App() {
+  var [count, setCount] = React.useState(0);
+
+  function increase() {
+    setCount(++count);
+  }
+  function decrease() {
+    setCount(--count);
+  }
+
+  return (
+    <div className="container">
+      <h1 className="count">{count}</h1>
+      <button onClick={decrease} className="count-btn">
+        -
+      </button>
+      <button onClick={increase} className="count-btn">
+        +
+      </button>
+    </div>
+  );
+}
+export default App;
